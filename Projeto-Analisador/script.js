38e9ed55517ff775736a5bc13e40d8b783ed6fc6
Luiz Filipe Reis <107514764+Yokkaii@users.noreply.github.com>
2022-09-09 17:08:34 -0300

    .

--- Projeto-Analisador/script.js
+++ Projeto-Analisador/script.js
@@ -22,9 +22,35 @@ function isLista(n, l) {
 
 
 function clique() {
-    if(isNumero(nun.value) && isLista(nun.value, valores)){
-        window.alert('Tudo OK!')
+    if(isNumero(nun.value) && !isLista(nun.value, valores)){
+        valores.push(Number(nun.value))
+        var item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado`
+        list.appendChild(item)
+        res.innerHTML = ''
     } else {
     window.alert('valor INVALIDO ou Ja encontrado na lista.')
+    }
+    nun.value = ''
+    nun.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0) {
+        window.alert('Adicione um Valor!')    
+    } else {
+        var tot = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        for(var pos in valores) {
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] > menor)
+            menorr = valores[pos]
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor foi ${maior}. </p>`
+        res.innerHTML += `<p>O menor valor foi ${menor}. </p>`
     }
 }
